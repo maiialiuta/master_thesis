a59466fefde12f31f7c689294873499634fb2dec
maiialiuta <150044368+maiialiuta@users.noreply.github.com>
2024-12-20 13:13:27 +0200

    Adds main app files

---
+++ app/src/metrics/categories/cohesion_category.py
@@ -0,0 +1,11 @@
+class CohesionCategory:
+
+    lcom = 0
+
+    # Metrics getters
+    def get_lcom(self):
+        return self.lcom
+
+    # Set value to the Metrics
+    def set_lcom(self, value):
+        self.lcom = value
